ef3623ba083b2a427a39dcffbb3f7c6b0278413c
JustynaFrackiewicz <justynaf12@op.pl>
2016-06-06 02:21:27 +0200

    Ostateczna wersja programu v1

--- src/pl/pw/edu/fizyka/pojava/lama/Controller.java
+++ src/pl/pw/edu/fizyka/pojava/lama/Controller.java
@@ -3,18 +3,16 @@ package pl.pw.edu.fizyka.pojava.lama;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.animation.KeyFrame;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Button;
 import javafx.scene.image.WritableImage;
 import javafx.scene.SnapshotParameters;
 import java.io.File;
@@ -24,12 +22,11 @@ import java.util.Collections;
 import java.util.List;
 import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.paint.Color;
 import javafx.scene.layout.Pane;
 import javafx.animation.Timeline;
-import javafx.scene.shape.StrokeLineCap;
 import javafx.util.Duration;
+import java.util.Random;
 
 public class Controller {
 
@@ -46,6 +43,8 @@ public class Controller {
     @FXML
     Button refreshChartButton;
     @FXML
+    Button animationButton;
+    @FXML
     ComboBox<String> coverNumberComboBox;
     @FXML
     ComboBox<String> coverDepthComboBox;
@@ -54,36 +53,37 @@ public class Controller {
     @FXML
     RadioButton linearScaleButton;
     @FXML
-    RadioButton loggharytmicScaleButton;
+    RadioButton logarithmicScaleButton;
 
     double coverDepth;
     double miFactor;
     double coverNumber;
-    double startGamma;  // początkowa liczba kwantów gamma
+    double startGamma;
+    String colorCover;
     final List<String> plutoniumLabel = Collections.unmodifiableList(Arrays.asList("Pluton (Pu)", "Plutonium (Pu)"));
     final List<String> uraniumLabel = Collections.unmodifiableList(Arrays.asList("Uran (U)", "Uranium (U)"));
     final List<String> cobaltLabel = Collections.unmodifiableList(Arrays.asList("Kobalt (Co)", "Cobalt (Co)"));
     final List<String> leadLabel = Collections.unmodifiableList(Arrays.asList("Ołów (Pb)", "Lead"));
     final List<String> copperLabel = Collections.unmodifiableList(Arrays.asList("Copper", "Miedź (Cu)"));
     final List<String> aluminumLabel = Collections.unmodifiableList(Arrays.asList("Aluminum", "Aluminium (Al)"));
-    double numberSteps;
-    WritableImage image;
-    File file;
-    ToggleGroup groupButtons;
-    Line coverLine;
-    Timeline loopBeforeCover;
-
+    ToggleGroup groupButtonsChart;
     ObservableList<String> optionsSourceMaterials;
     ObservableList<String> optionsCoverMaterials;
     ObservableList<String> optionsCoverDepth;
     ObservableList<String> optionsCoverNumber;
+    Timeline loopBeforeCover = new Timeline();
+    Timeline loopAfterCover = new Timeline();
+    Random generator = new Random();
+    boolean visibilityGamma;
+    WritableImage image;
+    File file;
 
     @FXML
-        // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        groupButtons = new ToggleGroup();
-        linearScaleButton.setToggleGroup(groupButtons);
-        loggharytmicScaleButton.setToggleGroup(groupButtons);
+        colorCover = "black";
+        groupButtonsChart = new ToggleGroup();
+        linearScaleButton.setToggleGroup(groupButtonsChart);
+        logarithmicScaleButton.setToggleGroup(groupButtonsChart);
         optionsSourceMaterials =
                 FXCollections.observableArrayList(
                         "Kobalt (Co)",
@@ -102,7 +102,8 @@ public class Controller {
                         "2 mm", "5 mm", "7 mm", "10 mm", "12 mm", "15 mm", "17 mm", "20 mm"
                 );
         coverDepthComboBox.setItems(optionsCoverDepth);
-        optionsCoverNumber = FXCollections.observableArrayList("1", "2", "3", "4", "5");
+        optionsCoverNumber = FXCollections.observableArrayList
+                ("1 przesłona", "2 przesłony", "3 przesłony", "4 przesłony", "5 przesłon");
         coverNumberComboBox.setItems(optionsCoverNumber);
         sourceMaterialComboBox.setItems(optionsSourceMaterials);
         coverDepthComboBox.setPromptText("Grubość przesłony");
@@ -111,8 +112,10 @@ public class Controller {
         sourceMaterialComboBox.setPromptText("Materiał źródła");
         lineChart.setTitle("Osłabienie promieniowania gamma");
         linearScaleButton.setText("Skala liniowa");
-        loggharytmicScaleButton.setText("Skala logarytmiczna");
+        logarithmicScaleButton.setText("Skala logarytmiczna");
         exportButton.setText("Eksportuj");
+        refreshChartButton.setText("Odśwież wykres");
+        animationButton.setText("Animacja");
     }
 
     @FXML
@@ -124,115 +127,122 @@ public class Controller {
                         "Lead"
                 );
         coverMaterialComboBox.setItems(optionsCoverMaterials);
-
         optionsSourceMaterials =
                 FXCollections.observableArrayList(
                         "Cobalt (Co)",
                         "Plutonium (Pu)",
                         "Uranium (U)"
                 );
-
         optionsCoverDepth =
                 FXCollections.observableArrayList(
                         "2 mm", "5 mm", "7 mm", "10 mm", "12 mm", "15 mm", "17 mm", "20 mm"
                 );
-        optionsCoverNumber = FXCollections.observableArrayList("1", "2", "3", "4", "5");
+        optionsCoverNumber = FXCollections.observableArrayList("1 cover", "2 covers", "3 covers", "4 covers", "5 covers");
+        coverNumberComboBox.setItems(optionsCoverNumber);
         coverDepthComboBox.setItems(optionsCoverDepth);
         sourceMaterialComboBox.setItems(optionsSourceMaterials);
         coverNumberComboBox.setItems(optionsCoverNumber);
+        animationButton.setText("Animation");
         coverDepthComboBox.setPromptText("Size of screen");
         coverNumberComboBox.setPromptText("Number of screens");
         coverMaterialComboBox.setPromptText("Material of screen");
         sourceMaterialComboBox.setPromptText("Material of source");
         lineChart.setTitle("The weakening of gamma radiation");
         linearScaleButton.setText("Linear scale");
-        loggharytmicScaleButton.setText("Logharytmic scale");
+        logarithmicScaleButton.setText("Logharitmic scale");
         exportButton.setText("Export");
         refreshChartButton.setText("Refresh chart");
     }
 
     @FXML
     void startAction() {
-        for(int s=0; s<20; s++) {
-            animationGamma.getChildren().clear();
-            beforeCoverAnimation();
-            afterCoverAnimation();
+        animationGamma.getChildren().clear();
+        Group coversGroup = new Group();
+        for(double z=0; z<coverNumber; z++) {
+            Rectangle cover = new Rectangle((95+coverDepth*z), 0, coverDepth, 255);
+            cover.setStroke(Color.web("black"));
+            cover.setStrokeWidth(1.5);
+            cover.setFill(Color.web(colorCover));
+            coversGroup.getChildren().addAll(cover);
         }
+            animationGamma.getChildren().setAll(coversGroup);
+            beforeAbsorption();
+            afterAbsorption();
     }
 
-    void beforeCoverAnimation() {
-        numberSteps=0;
-        //wstawienie bariery
-        animationGamma.getChildren().remove(coverLine);
-        coverLine = new Line(100, 5, 100, 250);
-        coverLine.setStrokeWidth(coverDepth);
-        animationGamma.getChildren().add(coverLine);
-        while (numberSteps < 1000) {
-            //kulki - poczatkowa liczba 15 - rozpatrujemy jedna paczke kwantowa
-            Circle circle = new Circle(1, Color.BLUE);
-            circle.relocate(10*Math.random(), Math.random()*250);
-            numberSteps ++;
-            animationGamma.getChildren().add(circle);
+    void beforeAbsorption() {
+        int numberSteps = 0;
+        while (numberSteps < 200) {
+            Circle gammaPortion = new Circle(2, Color.BLUE);
+            gammaPortion.relocate(10 * Math.random(), Math.random() * 250);
+            animationGamma.getChildren().add(gammaPortion);
             loopBeforeCover = new Timeline(new KeyFrame(Duration.millis(30), new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(final ActionEvent t) {
-                    if (circle.getLayoutX() < 95) {
-                        circle.setLayoutX(circle.getLayoutX() + +Math.random() + 0.6);
-                        circle.setLayoutY(circle.getLayoutY() + Math.random() - 0.5);
+                    if (gammaPortion.getLayoutX() < 95) {
+                        gammaPortion.setLayoutX(gammaPortion.getLayoutX() + Math.random() + 0.6);
+                        gammaPortion.setLayoutY(gammaPortion.getLayoutY() + Math.random() - 0.5);
+                    } else if (gammaPortion.getLayoutX() > 95 && gammaPortion.getLayoutX() < (95 + coverDepth * coverNumber)) {
+                        visibilityGamma = generator.nextBoolean();
+                        gammaPortion.setVisible(visibilityGamma);
+                        gammaPortion.setLayoutX(gammaPortion.getLayoutX() + Math.random() + 0.6);
+                        gammaPortion.setLayoutY(gammaPortion.getLayoutY() + Math.random() - 0.5);
+                    } else if (gammaPortion.getLayoutX() == (95 + coverDepth * coverNumber)) {
+                        gammaPortion.setVisible(false);
+                    } else {
+                        gammaPortion.setVisible(false);
                     }
-                    else
-                        circle.setVisible(false);
                 }
             }));
-           // loopBeforeCover.setDelay(Duration.millis(50));
             loopBeforeCover.setCycleCount(Timeline.INDEFINITE);
-            loopBeforeCover.play();}
+            loopBeforeCover.play();
+            numberSteps++;
+        }
     }
 
-    void afterCoverAnimation() {
-        numberSteps=0;
-        while (numberSteps < 1000*Math.exp((-1)*miFactor*coverDepth)) {
-            //kulki - poczatkowa liczba 15 - rozpatrujemy jedna paczke kwantowa
-            Circle circle = new Circle(1, Color.BLUE);
-            circle.setVisible(false);
-            circle.relocate(91 + coverDepth + Math.random(), Math.random()*250);
-            numberSteps ++;
-            animationGamma.getChildren().addAll(circle);
-            Timeline loopAfterCover = new Timeline(new KeyFrame(Duration.millis(30), new EventHandler<ActionEvent>() {
-
-                @Override
-                public void handle(final ActionEvent t) {
-                    if (circle.getLayoutX() > 91+coverDepth && circle.getLayoutX() < 400 ) {
-                        circle.setVisible(true);
-                        circle.setLayoutX(circle.getLayoutX() + + Math.random() + 0.6);
-                        circle.setLayoutY(circle.getLayoutY() + Math.random()-0.5);
-                    } else
-                        circle.setVisible(false);
+    void afterAbsorption() {
+        int numberSteps=0;
+        while( numberSteps < 200*(Math.exp((-1)*miFactor*coverNumber*coverDepth))) {
+                Circle gammaPortionSurvivor = new Circle(2, Color.BLUE);
+                gammaPortionSurvivor.setVisible(false);
+                gammaPortionSurvivor.relocate(95 + coverDepth * coverNumber, Math.random() * 250);
+                animationGamma.getChildren().add(gammaPortionSurvivor);
+                loopAfterCover = new Timeline(new KeyFrame(Duration.millis(30), new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(final ActionEvent t) {
+                        if ((gammaPortionSurvivor.getLayoutX() > (95 + coverDepth * coverNumber))
+                                && gammaPortionSurvivor.getLayoutX() < 350) {
+                            gammaPortionSurvivor.setVisible(true);
+                            gammaPortionSurvivor.setLayoutX(gammaPortionSurvivor.getLayoutX() + Math.random() + 0.6);
+                            gammaPortionSurvivor.setLayoutY(gammaPortionSurvivor.getLayoutY() + Math.random() - 0.5);
+                        }
+                        else
+                            gammaPortionSurvivor.setVisible(false);
+                    }
+                }));
+                loopAfterCover.setCycleCount(Timeline.INDEFINITE);
+                loopAfterCover.setDelay(Duration.millis(2100+20*coverNumber*coverDepth));
+                loopAfterCover.play();
+                numberSteps++;
                 }
-            }));
-            loopAfterCover.setCycleCount(Timeline.INDEFINITE);
-            loopAfterCover.setDelay(Duration.millis(2300));
-            loopAfterCover.play();}
-    }
-
-    @FXML
-    void stopAction() {
-        //Stop Animacja
-    }
+        }
 
     @FXML
     void exportAction() {
         image = lineChart.snapshot(new SnapshotParameters(), null);
         file = new File("chart.png");
         try {
+
             ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+
         } catch (IOException e) {
-            // TODO: handle exception here
+
         }
     }
 
     @FXML
     void chooseSourceMaterial() {
+
         if (cobaltLabel.contains(sourceMaterialComboBox.getValue()))
             startGamma = 40000000000000d;
 
@@ -241,30 +251,42 @@ public class Controller {
 
         if (uraniumLabel.contains(sourceMaterialComboBox.getValue()))
             startGamma = 40000;
+
     }
 
     @FXML
     void chooseCoverMaterial() {
-        if (leadLabel.contains(coverMaterialComboBox.getValue()))
+        if (leadLabel.contains(coverMaterialComboBox.getValue())) {
             miFactor = 0.15;
-        if (copperLabel.contains(coverMaterialComboBox.getValue()))
+            colorCover = "gray";
+        }
+        if (copperLabel.contains(coverMaterialComboBox.getValue())) {
             miFactor = 0.093;
-        if (aluminumLabel.contains(coverMaterialComboBox.getValue()))
+            colorCover = "orange";
+        }
+        if (aluminumLabel.contains(coverMaterialComboBox.getValue())) {
             miFactor = 0.049;
+            colorCover = "silver";
+        }
     }
 
     @FXML
     void chooseNumberCovers() {
         if (coverNumberComboBox.getValue() != null){
-            if (coverNumberComboBox.getValue().equals("1"))
+            if (coverNumberComboBox.getValue().equals("1 przesłona")
+                    || coverNumberComboBox.getValue().equals("1 cover"))
                 coverNumber = 1;
-            if (coverNumberComboBox.getValue().equals("2"))
+            if (coverNumberComboBox.getValue().equals("2 przesłony")
+                    || coverNumberComboBox.getValue().equals("2 covers"))
                 coverNumber = 2;
-            if (coverNumberComboBox.getValue().equals("3"))
+            if (coverNumberComboBox.getValue().equals("3 przesłony")
+                    || coverNumberComboBox.getValue().equals("3 covers"))
                 coverNumber = 3;
-            if (coverNumberComboBox.getValue().equals("4"))
+            if (coverNumberComboBox.getValue().equals("4 przesłony")
+                    || coverNumberComboBox.getValue().equals("4 covers"))
                 coverNumber = 4;
-            if (coverNumberComboBox.getValue().equals("5"))
+            if (coverNumberComboBox.getValue().equals("5 przesłon")
+                    || coverNumberComboBox.getValue().equals("5 covers"))
                 coverNumber = 5;
         }
     }
@@ -293,22 +315,22 @@ public class Controller {
 
     @FXML
     void linearScale(){
-        double dii = 0.01; //krok dodawania do x
-        double I_o = startGamma; //wartosc poczatkowa natezenia (przykladowa)
+        double dx = 0.01;
+        double I_o = startGamma;
         double x=0;
-        double y=1;
+        double y;
         ObservableList<XYChart.Data<Double, Double>> lineChartData;
         lineChartData = FXCollections.observableArrayList();
         ObservableList<XYChart.Series<Double, Double>> lineChartSeries;
         lineChartSeries= FXCollections.observableArrayList();
 
         while (x <= coverDepth*coverNumber) {
-
-            double argument=((-1)*miFactor*x);
+            double argument=((-1)*miFactor*coverNumber*x);
             y = I_o * Math.exp(argument);
             lineChartData.add(new LineChart.Data(x,y));
-            x += dii;
+            x += dx;
         }
+
         lineChartSeries.add(new LineChart.Series(lineChartData));
         lineChart.setData(lineChartSeries);
         lineChart.setCreateSymbols(false);
@@ -316,37 +338,34 @@ public class Controller {
 
     @FXML
     void logarithmicScale(){
-        double dii = 0.01; //krok dodawania do x
-        double I_o = startGamma; //wartosc poczatkowa natezenia (przykladowa)
+        double dx = 0.01;
+        double I_o = startGamma;
         double x=0;
         double y=0;
         ObservableList<XYChart.Data<Double, Double>> lineChartData;
         lineChartData = FXCollections.observableArrayList();
         ObservableList<XYChart.Series<Double, Double>> lineChartSeries;
-
         lineChartSeries= FXCollections.observableArrayList();
 
         while (x <= coverDepth * coverNumber) {
-            {
                 if ( y>=0 ) {
                     double argument = (-1) * miFactor  * x;
                     y = Math.log(I_o * Math.exp(argument));
                     lineChartData.add(new LineChart.Data(x, y));
-                    x += dii;
+                    x += dx;
                 }
                 else
                     break;
-            }
         }
+
         lineChartSeries.add(new LineChart.Series(lineChartData));
         lineChart.setData(lineChartSeries);
-
         lineChart.setCreateSymbols(false);
     }
 
     @FXML
     void refreshChart() {
         if (linearScaleButton.isSelected()) { linearScale(); }
-        if (loggharytmicScaleButton.isSelected()) { logarithmicScale(); }
+        if (logarithmicScaleButton.isSelected()) { logarithmicScale(); }
     }
 }
